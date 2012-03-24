b2d7eec7673e11f10f524817debc1fc313b5d7ba
Chris Fordham <chris@xhost.com.au>
2012-03-24 14:02:31 +1100

    Enable/start ossec-hids service only.

--- cookbooks/ossec/recipes/default.rb
+++ cookbooks/ossec/recipes/default.rb
@@ -15,9 +15,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# install ossec (ossec-hids)
 include_recipe "ossec::install"
 
-service "ossec" do
-  supports :status => true, :restart => true
+# ensure the ossec-hids service is enabled and started
+service "ossec-hids" do
   action [:enable, :start]
 end
